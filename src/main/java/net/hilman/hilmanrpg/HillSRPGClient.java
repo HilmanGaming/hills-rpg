4621add406f9ff49479ee531ffcd7e511feec863
HilmanGaming <73045229+HilmanGaming@users.noreply.github.com>
2026-08-28 15:10:37 +0700

    Initial commit

---
+++ src/main/java/net/hilman/hilmanrpg/HillSRPGClient.java
@@ -0,0 +1,10 @@
+package net.hilman.hilmanrpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HillSRPGClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hilman/hilmanrpg/HillSRPGDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hilman.hilmanrpg;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HillSRPGDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
