d0a209336e9807d611105b5cc0677d00b288cf6b
HilmanGaming <73045229+HilmanGaming@users.noreply.github.com>
2026-09-11 12:54:49 +0700

    nyoh

--- src/main/java/net/hilman/hilmanrpg/HillSRPG.java
+++ src/main/java/net/hilman/hilmanrpg/HillSRPG.java
@@ -3,10 +3,13 @@ package net.hilman.hilmanrpg;
 import net.fabricmc.api.ModInitializer;
 
 import net.hilman.hilmanrpg.item.ModItems;
+import net.hilman.hilmanrpg.rpgclass.ClassArmorLockHandler;
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.github.theredbrain.rpgclassselection.registry.RPGClassSelectionConfigs;
 
 public class HillSRPG implements ModInitializer {
 	public static final String MOD_ID = "hills-rpg";
@@ -19,8 +22,10 @@ public class HillSRPG implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.register();
+		ClassArmorLockHandler.register();
 
 		LOGGER.info("Bisa anjay bangke woooooooooooooooo!!!!");
+		RPGClassSelectionConfigs.SERVER_CONFIG.firstJoinScreenSettings.enable_first_join_class_selection = false;
 	}
 
 	public static Identifier id(String path) {
